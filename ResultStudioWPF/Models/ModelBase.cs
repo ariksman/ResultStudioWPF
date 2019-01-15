1f8595321b868b4aa4bb5ba75c6af557c9119700
ariksman <10255950+ariksman@users.noreply.github.com>
2019-01-15 15:05:10 +0200

    general code formatting improvement

--- ResultStudioWPF/Models/ModelBase.cs
+++ ResultStudioWPF/Models/ModelBase.cs
@@ -2,86 +2,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
 
 namespace ResultStudioWPF.Models
 {
-    // Guide:  https://www.codeproject.com/Tips/876349/WPF-Validation-using-INotifyDataErrorInfo
+  // Guide:  https://www.codeproject.com/Tips/876349/WPF-Validation-using-INotifyDataErrorInfo
 
-    public class ModelBase : INotifyPropertyChanged, INotifyDataErrorInfo
+  public class ModelBase : INotifyPropertyChanged, INotifyDataErrorInfo
+  {
+    #region Property changed
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    // making notifypropertychanged easier http://jesseliberty.com/2012/06/28/c-5making-inotifypropertychanged-easier/
+    protected void NotifyPropertyChanged(Action<bool> message, [CallerMemberName] string caller = "")
     {
-        #region Property changed
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // making notifypropertychanged easier http://jesseliberty.com/2012/06/28/c-5making-inotifypropertychanged-easier/
-        protected void NotifyPropertyChanged(Action<bool> message, [CallerMemberName] string caller = "")
-        {
-            if (this.PropertyChanged != null)
-            {
-                // property changed
-                this.PropertyChanged(this, new PropertyChangedEventArgs(caller));
-                // send app message (mvvm light toolkit)
-                if (message != null)
-                    message(this.IsValid);
-            }
-        }
-        #endregion
-
-        #region Notify data error
-        private Dictionary<string, List<string>> _errors = new Dictionary<string, List<string>>();
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        // get errors by property
-        public IEnumerable GetErrors(string propertyName)
-        {
-            if (propertyName == null) return null;
-            return this._errors.ContainsKey(propertyName) ? this._errors[propertyName] : null;
-        }
-
-        // has errors
-        private bool _hasErrors;
-        public bool HasErrors
-        {
-            get
-            {
-                if (_errors == null) _errors = new Dictionary<string, List<string>>();
-                return (this._errors.Count > 0);
-            }
-            set { _hasErrors = value; }
-        }
-
-        // object is valid
-        private bool _isValid;
-        public bool IsValid
-        {
-            get { return !this.HasErrors; }
-            set { _isValid = value; }
-        }
-
-        public void AddError(string propertyName, string error)
-        {
-            // Add error to list
-            this._errors[propertyName] = new List<string>() { error };
-            this.NotifyErrorsChanged(propertyName);
-        }
-
-        public void RemoveError(string propertyName)
-        {
-            // remove error
-            if (this._errors.ContainsKey(propertyName))
-                this._errors.Remove(propertyName);
-            this.NotifyErrorsChanged(propertyName);
-        }
-
-        public void NotifyErrorsChanged(string propertyName)
-        {
-            // Notify
-            if (this.ErrorsChanged != null)
-                this.ErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
-        }
-
-        #endregion
+      if (PropertyChanged != null)
+      {
+        // property changed
+        PropertyChanged(this, new PropertyChangedEventArgs(caller));
+        // send app message (mvvm light toolkit)
+        message?.Invoke(this.IsValid);
+      }
     }
+
+    #endregion
+
+    #region Notify data error
+
+    private Dictionary<string, List<string>> _errors = new Dictionary<string, List<string>>();
+    public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+    // get errors by property
+    public IEnumerable GetErrors(string propertyName)
+    {
+      if (propertyName == null) return null;
+      return _errors.ContainsKey(propertyName) ? _errors[propertyName] : null;
+    }
+
+    // has errors
+    private bool _hasErrors;
+
+    public bool HasErrors
+    {
+      get
+      {
+        if (_errors == null) _errors = new Dictionary<string, List<string>>();
+        return (_errors.Count > 0);
+      }
+      set { _hasErrors = value; }
+    }
+
+    // object is valid
+    private bool _isValid;
+
+    public bool IsValid
+    {
+      get { return !this.HasErrors; }
+      set { _isValid = value; }
+    }
+
+    public void AddError(string propertyName, string error)
+    {
+      // Add error to list
+      _errors[propertyName] = new List<string>() {error};
+      NotifyErrorsChanged(propertyName);
+    }
+
+    public void RemoveError(string propertyName)
+    {
+      // remove error
+      if (_errors.ContainsKey(propertyName))
+        _errors.Remove(propertyName);
+      NotifyErrorsChanged(propertyName);
+    }
+
+    public void NotifyErrorsChanged(string propertyName)
+    {
+      // Notify
+      if (ErrorsChanged != null)
+        ErrorsChanged(this, new DataErrorsChangedEventArgs(propertyName));
+    }
+
+    #endregion
+  }
 }
--- ResultStudioWPF/Models/SubFrame.cs
+++ ResultStudioWPF/Models/SubFrame.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using GalaSoft.MvvmLight.Messaging;
-using OxyPlot;
-using ResultStudioWPF.Helpers;
-using ResultStudioWPF.Messages;
+﻿using ResultStudioWPF.Helpers;
 using ResultStudioWPF.ViewModels;
 
 namespace ResultStudioWPF.Models
@@ -20,98 +10,8 @@ namespace ResultStudioWPF.Models
             
         }
 
-        #region Public methods
-        public void RefreshAllProperties()
-        {
-            foreach (var prop in this.GetType().GetProperties())
-                NotifyPropertyChanged(null, prop.Name);
-        }
-
-        public void CheckAllTolerances()
-        {
-            foreach (var propertyInfo in this.GetType().GetProperties())
-            {
-                if (propertyInfo.Name == "Value")
-                {
-                    CheckValueTolerance(this.Value);
-                }
-            }
-        }
-
-        #endregion
-
-        #region Private methods
-        private void CheckValueTolerance(double value)
-        {
-            if (this.AxisName == Constants.MeasurementAxis.X)
-            {
-                var xTolerance = new ViewModelLocator().SettingsViewModel.XAxisTolerance;
-                var xReference = new ViewModelLocator().SettingsViewModel.XAxisReference;
-
-                if (_value > xReference + xTolerance)
-                {
-
-                    AddError("Value", "Given measurement value (" + _value + ") is higher than tolerance " + xTolerance + " allows");
-                }
-                else if (value < xReference - xTolerance)
-                {
-                    AddError("Value",
-                        "Given measurement value (" + _value + ") is lower than tolerance " + xTolerance +
-                        " allows");
-                }
-                else
-                {
-                    RemoveError("Value");
-                }
-            }
-            if (this.AxisName == Constants.MeasurementAxis.Y)
-            {
-                var yTolerance = new ViewModelLocator().SettingsViewModel.YAxisTolerance;
-                var yReference = new ViewModelLocator().SettingsViewModel.YAxisReference;
-
-                if (_value > yReference + yTolerance)
-                {
-
-                    AddError("Value", "Given measurement value (" + _value + ") is higher than tolerance " + yTolerance + " allows");
-                }
-                else if (value < yReference - yTolerance)
-                {
-                    AddError("Value",
-                        "Given measurement value (" + _value + ") is lower than tolerance " + yTolerance +
-                        " allows");
-                }
-                else
-                {
-                    RemoveError("Value");
-                }
-            }
-
-            if (this.AxisName == Constants.MeasurementAxis.Z)
-            {
-                var zTolerance = new ViewModelLocator().SettingsViewModel.ZAxisTolerance;
-                var zReference = new ViewModelLocator().SettingsViewModel.ZAxisReference;
-
-                if (_value > zReference + zTolerance)
-                {
-
-                    AddError("Value", "Given measurement value (" + _value + ") is higher than tolerance " + zTolerance + " allows");
-                }
-                else if (value < zReference - zTolerance)
-                {
-                    AddError("Value",
-                        "Given measurement value (" + _value + ") is lower than tolerance " + zTolerance +
-                        " allows");
-                }
-                else
-                {
-                    RemoveError("Value");
-                }
-            }
-        }
-        #endregion
-
-        #region Public roperties
-        private int _measurementNumber;
+    #region Public roperties
+    private int _measurementNumber;
         public int MeasurementNumber
         {
             get { return _measurementNumber; }
@@ -147,6 +47,96 @@ namespace ResultStudioWPF.Models
                 NotifyPropertyChanged(null);
             }
         }
-        #endregion
+    #endregion
+
+    #region Public methods
+    public void RefreshAllProperties()
+    {
+      foreach (var prop in GetType().GetProperties())
+        NotifyPropertyChanged(null, prop.Name);
     }
+
+    public void CheckAllTolerances()
+    {
+      foreach (var propertyInfo in GetType().GetProperties())
+      {
+        if (propertyInfo.Name == "Value")
+        {
+          CheckValueTolerance(Value);
+        }
+      }
+    }
+
+    #endregion
+
+    #region Private methods
+    private void CheckValueTolerance(double value)
+    {
+      if (AxisName == Constants.MeasurementAxis.X)
+      {
+        var xTolerance = new ViewModelLocator().SettingsViewModel.XAxisTolerance;
+        var xReference = new ViewModelLocator().SettingsViewModel.XAxisReference;
+
+        if (_value > xReference + xTolerance)
+        {
+
+          AddError("Value", "Given measurement value (" + _value + ") is higher than tolerance " + xTolerance + " allows");
+        }
+        else if (value < xReference - xTolerance)
+        {
+          AddError("Value",
+              "Given measurement value (" + _value + ") is lower than tolerance " + xTolerance +
+              " allows");
+        }
+        else
+        {
+          RemoveError("Value");
+        }
+      }
+      if (AxisName == Constants.MeasurementAxis.Y)
+      {
+        var yTolerance = new ViewModelLocator().SettingsViewModel.YAxisTolerance;
+        var yReference = new ViewModelLocator().SettingsViewModel.YAxisReference;
+
+        if (_value > yReference + yTolerance)
+        {
+
+          AddError("Value", "Given measurement value (" + _value + ") is higher than tolerance " + yTolerance + " allows");
+        }
+        else if (value < yReference - yTolerance)
+        {
+          AddError("Value",
+              "Given measurement value (" + _value + ") is lower than tolerance " + yTolerance +
+              " allows");
+        }
+        else
+        {
+          RemoveError("Value");
+        }
+      }
+
+      if (AxisName == Constants.MeasurementAxis.Z)
+      {
+        var zTolerance = new ViewModelLocator().SettingsViewModel.ZAxisTolerance;
+        var zReference = new ViewModelLocator().SettingsViewModel.ZAxisReference;
+
+        if (_value > zReference + zTolerance)
+        {
+
+          AddError("Value", "Given measurement value (" + _value + ") is higher than tolerance " + zTolerance + " allows");
+        }
+        else if (value < zReference - zTolerance)
+        {
+          AddError("Value",
+              "Given measurement value (" + _value + ") is lower than tolerance " + zTolerance +
+              " allows");
+        }
+        else
+        {
+          RemoveError("Value");
+        }
+      }
+    }
+    #endregion
+  }
 }
