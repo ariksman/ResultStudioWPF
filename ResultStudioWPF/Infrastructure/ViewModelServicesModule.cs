712275f2dfcc44d1b119b499ac5af1629a7a04ce
Antti Riksman <antti.riksman@gmail.com>
2019-09-08 19:49:34 +0300

    namespace change

---
+++ ResultStudioWPF/Infrastructure/ViewModelServicesModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using ResultStudioWPF.Application.Interfaces;
+using ResultStudioWPF.Models.Services;
+using ResultStudioWPF.ViewModels;
+using ResultStudioWPF.ViewModels.Services;
+
+namespace ResultStudioWPF.Infrastructure
+{
+  public class ViewModelServicesModule : Module
+  {
+    protected override void Load(ContainerBuilder builder)
+    {
+      builder.RegisterType<DataFileReader>().As<IDataFileReader>();
+      builder.RegisterType<AnalyzeDataSet>().As<IAnalyseDataSet>();
+      builder.RegisterType<DataCreator>().As<IDataCreator>();
+
+      builder.RegisterType<SettingsViewModel>().AsSelf().SingleInstance();
+      builder.RegisterType<ResultsViewModel>().AsSelf().SingleInstance();
+    }
+  }
+}
