99808939bd0f61c664c3d4b45200bca9dab10a0e
Antti Riksman <antti.riksman@gmail.com>
2019-09-08 23:45:39 +0300

    new types and validator service

---
+++ ResultStudioWPF.Domain/DomainModel/ValueObjects/Tolerance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using ResultStudioWPF.Domain.DDD;
+
+namespace ResultStudioWPF.Domain
+{
+  public class Tolerance : ValueObject
+  {
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
+
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+      yield return X;
+      yield return Y;
+      yield return Z;
+    }
+  }
+}
---
+++ ResultStudioWPF.Domain/Services/ValueValidatorService.cs
@@ -0,0 +1,17 @@
+﻿using ResultStudioWPF.Domain.DomainModel.Enumerations;
+
+namespace ResultStudioWPF.Domain.Services
+{
+  public class ValueValidatorService : IMeasurementPointValueValidator
+  {
+    public bool IsValid(MeasurementAxisType measurementAxisType, Tolerance tolerance, Reference reference)
+    {
+      return measurementAxisType.IsValid(tolerance, reference);
+    }
+  }
+
+  public interface IMeasurementPointValueValidator
+  {
+    bool IsValid(MeasurementAxisType measurementAxisType, Tolerance tolerance, Reference reference);
+  }
+}
