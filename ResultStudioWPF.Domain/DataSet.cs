862b841fea2b0dc4e449d5e32d2fba3ff8217243
Antti Riksman <antti.riksman@gmail.com>
2019-09-08 22:39:30 +0300

    introducing domain classes

---
+++ ResultStudioWPF.Domain/DataSet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ResultStudioWPF.Domain.DDD;
+
+namespace ResultStudioWPF.Domain
+{
+  public class DataSet : AggregateRoot
+  {
+    public string Name { get; set; }
+    public IReadOnlyList<MeasurementPoint> MeasurementPoints { get; set; }
+  }
+}
---
+++ ResultStudioWPF.Domain/MeasurementPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using ResultStudioWPF.Domain.DDD;
+
+namespace ResultStudioWPF.Domain
+{
+  public class MeasurementPoint : ValueObject
+  {
+
+    public int IndexNumber { get; set; }  
+
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
