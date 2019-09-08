bd51415ac0d133308522cf3ccf92557505580c87
Antti Riksman <antti.riksman@gmail.com>
2019-09-08 22:36:02 +0300

    Added basic types for domain driven design

---
+++ ResultStudioWPF.Domain/DDD/AggregateRoot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace ResultStudioWPF.Domain.DDD
+{
+
+  /// <summary> An aggregate root base class. Explicitly mark aggregate roots and implement their common logic </summary>
+  public abstract class AggregateRoot : Entity
+  {
+    private readonly List<IDomainEvent> _domainEvents = new List<IDomainEvent>();
+    public virtual IReadOnlyList<IDomainEvent> DomainEvents => _domainEvents;
+
+    protected virtual void AddDomainEvent(IDomainEvent newEvent)
+    {
+      _domainEvents.Add(newEvent);
+    }
+
+    public virtual void ClearEvents()
+    {
+      _domainEvents.Clear();
+    }
+  }
+}
---
+++ ResultStudioWPF.Domain/DDD/Specification.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ResultStudioWPF.Domain.DDD
+{
+    internal sealed class IdentitySpecification<T> : Specification<T>
+    {
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            return x => true;
+        }
+    }
+
+    public abstract class Specification<T>
+    {
+        public static readonly Specification<T> All = new IdentitySpecification<T>();
+
+        public bool IsSatisfiedBy(T entity)
+        {
+            Func<T, bool> predicate = ToExpression().Compile();
+            return predicate(entity);
+        }
+
+        public abstract Expression<Func<T, bool>> ToExpression();
+
+        public Specification<T> And(Specification<T> specification)
+        {
+            if (this == All)
+                return specification;
+            if (specification == All)
+                return this;
+
+            return new AndSpecification<T>(this, specification);
+        }
+
+        public Specification<T> Or(Specification<T> specification)
+        {
+            if (this == All || specification == All)
+                return All;
+
+            return new OrSpecification<T>(this, specification);
+        }
+
+        public Specification<T> Not()
+        {
+            return new NotSpecification<T>(this);
+        }
+    }
+
+    internal sealed class AndSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> _left;
+        private readonly Specification<T> _right;
+
+        public AndSpecification(Specification<T> left, Specification<T> right)
+        {
+            _right = right;
+            _left = left;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            Expression<Func<T, bool>> leftExpression = _left.ToExpression();
+            Expression<Func<T, bool>> rightExpression = _right.ToExpression();
+
+            var invokedExpression = Expression.Invoke(rightExpression, leftExpression.Parameters);
+
+            return (Expression<Func<T, Boolean>>)Expression.Lambda(Expression.AndAlso(leftExpression.Body, invokedExpression), leftExpression.Parameters);
+        }
+    }
+
+    internal sealed class OrSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> _left;
+        private readonly Specification<T> _right;
+
+        public OrSpecification(Specification<T> left, Specification<T> right)
+        {
+            _right = right;
+            _left = left;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            Expression<Func<T, bool>> leftExpression = _left.ToExpression();
+            Expression<Func<T, bool>> rightExpression = _right.ToExpression();
+
+            var invokedExpression = Expression.Invoke(rightExpression, leftExpression.Parameters);
+
+            return (Expression<Func<T, Boolean>>)Expression.Lambda(Expression.OrElse(leftExpression.Body, invokedExpression), leftExpression.Parameters);
+        }
+    }
+
+    internal sealed class NotSpecification<T> : Specification<T>
+    {
+        private readonly Specification<T> _specification;
+
+        public NotSpecification(Specification<T> specification)
+        {
+            _specification = specification;
+        }
+
+        public override Expression<Func<T, bool>> ToExpression()
+        {
+            Expression<Func<T, bool>> expression = _specification.ToExpression();
+            UnaryExpression notExpression = Expression.Not(expression.Body);
+
+            return Expression.Lambda<Func<T, bool>>(notExpression, expression.Parameters.Single());
+        }
+    }
+}
