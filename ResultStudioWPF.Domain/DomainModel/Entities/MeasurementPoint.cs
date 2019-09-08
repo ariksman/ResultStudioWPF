425371b0259cad4c9d8267fb1ea0d79a427bc195
Antti Riksman <antti.riksman@gmail.com>
2019-09-08 23:43:14 +0300

    file moved

---
+++ ResultStudioWPF.Domain/DomainModel/Entities/MeasurementPoint.cs
@@ -0,0 +1,12 @@
+﻿using ResultStudioWPF.Domain.DDD;
+
+namespace ResultStudioWPF.Domain.DomainModel.Entities
+{
+  public class MeasurementPoint : Entity
+  {
+
+    public int IndexNumber { get; set; }
+    public Tolerance Tolerance { get; set; }
+    public Reference Reference { get; set; }
+  }
+}
