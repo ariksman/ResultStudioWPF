98b07b0628f0264a5a96d4a0fc80e9174f2c367a
Antti Riksman <antti.riksman@gmail.com>
2019-09-14 00:49:36 +0300

    Introduced new use-case to fetch data from file via a query

---
+++ ResultStudioWPF.Application/UseCaseHandlers/GetDataSetFromFileHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using ResultStudioWPF.Application.Interfaces;
+using ResultStudioWPF.Common.CQS;
+using ResultStudioWPF.Domain.DomainModel.Entities;
+using ResultStudioWPF.Domain.DomainModel.ValueObjects;
+using ResultStudioWPF.Domain.Interfaces;
+using ResultStudioWPF.Domain.UseCases.DataSet;
+
+namespace ResultStudioWPF.Application.UseCaseHandlers
+{
+  public class GetDataSetFromFileHandler : IQueryHandler<GetDataSetFromFile, Result<List<MeasurementPoint>>>
+  {
+    private readonly Func<FilePath, IDataFileReader> _dataFileReaderFunc;
+    private readonly IFileDialogProvider _fileDialogProvider;
+
+    public GetDataSetFromFileHandler(Func<FilePath, IDataFileReader> dataFileReaderFunc, IFileDialogProvider fileDialogProvider)
+    {
+      _dataFileReaderFunc = dataFileReaderFunc ?? throw new ArgumentException(nameof(dataFileReaderFunc));
+      _fileDialogProvider = fileDialogProvider ?? throw new ArgumentException(nameof(fileDialogProvider));
+    }
+
+    public Result<List<MeasurementPoint>> Handle(GetDataSetFromFile query)
+    {
+      var filter = "Text|*.txt|All|*.*";
+      var files = _fileDialogProvider.GetFilePaths(filter);
+      var file = FilePath.Create(files.FirstOrDefault());
+
+      if (file.IsFailure) return Result.Fail<List<MeasurementPoint>>("Error while getting the file path");
+
+      var reader = _dataFileReaderFunc(file.Value);
+      var data = reader.ReadFileIntoDataSet();
+
+      return Result.Ok(data);
+    }
+
+    public Task<Result<List<MeasurementPoint>>> HandleAsync(GetDataSetFromFile query)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ ResultStudioWPF.Domain/UseCases/DataSet/GetDataSetFromFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ResultStudioWPF.Common.CQS;
+
+namespace ResultStudioWPF.Domain.UseCases.DataSet
+{
+  public class GetDataSetFromFile : IQuery
+  {
+    public IProgress<int> Progress { get; }
+
+    public GetDataSetFromFile(IProgress<int> progress = null)
+    {
+      Progress = progress;
+    }
+  }
+}
