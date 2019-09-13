21ad570dd4860cc55b1f7627fd473c4491824673
Antti Riksman <antti.riksman@gmail.com>
2019-09-14 00:51:17 +0300

    Update MeasurementMappingProfile.cs

--- ResultStudioWPF/Mappers/Profiles/MeasurementMappingProfile.cs
+++ ResultStudioWPF/Mappers/Profiles/MeasurementMappingProfile.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AutoMapper;
-using ResultStudioWPF.Application.Interfaces;
-using ResultStudioWPF.Domain.DomainModel;
+﻿using AutoMapper;
 using ResultStudioWPF.Domain.DomainModel.Entities;
 using ResultStudioWPF.Domain.Interfaces;
 using ResultStudioWPF.Models;
@@ -17,6 +10,7 @@ namespace ResultStudioWPF.Mappers.Profiles
     public MeasurementMappingProfile()
     {
       CreateMap<IMeasurementPoint, MeasurementPointViewModel>().ReverseMap();
+      CreateMap<MeasurementPoint, MeasurementPointViewModel>().ReverseMap();
     }
   }
 }
